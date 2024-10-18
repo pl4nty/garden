41dfe00eb150bf2210b557264fcb0ac880b5130b
Tom Plant <tom@tplant.com.au>
2024-10-18 23:12:22 +1100

    feat: migrate to quartz (#12)
    * theme inspired by chadly.net
    * tweaks to footer and homepage

---
+++ quartz.config.ts
@@ -0,0 +1,94 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4.0 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "tplant.com.au",
+    pageTitleSuffix: "",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "cabin",
+    },
+    locale: "en-GB",
+    baseUrl: "garden.tplant.com.au",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "modified",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Titillium Web",
+        body: "Titillium Web",
+        code: "Fira Code",
+      },
+      colors: {
+        lightMode: {
+          light: "#FFFFFF", // white for background
+          lightgray: "#F8F8F8", // very light gray for subtle contrast
+          gray: "#B0B0B0", // lighter gray for standard contrast
+          darkgray: "#505050", // medium gray for text
+          dark: "#303030", // darker gray for high contrast elements
+          secondary: "#2E7D32", // vibrant green for highlights
+          tertiary: "#388E3C", // fresh green for accents
+          highlight: "rgba(46, 125, 50, 0.15)", // green highlight
+          textHighlight: "rgba(46, 125, 50, 0.15)",
+        },
+        darkMode: {
+          light: "#101010", // very dark gray for background
+          lightgray: "#181818", // dark gray for subtle contrast
+          gray: "#7a8882", // medium dark gray for standard contrast
+          darkgray: "#B0D69D", // lighter green-gray for text
+          dark: "#E0F0E9", // very light green-gray for high contrast elements
+          secondary: "#66BB6A", // medium green-gray for links and highlights
+          tertiary: "#81C784", // fresh green for success messages and accents
+          highlight: "rgba(102, 187, 106, 0.15)", // medium green-gray highlight
+          textHighlight: "rgba(102, 187, 106, 0.15)",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["git", "frontmatter", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
---
+++ quartz.layout.ts
@@ -0,0 +1,64 @@
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
+
+// components shared across all pages
+export const sharedPageComponents: SharedLayout = {
+  head: Component.Head(),
+  header: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'pl4nty/garden',
+        // from data-repo-id
+        repoId: 'R_kgDOLr2SEQ',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOLr2SEc4Cftp8',
+        reactionsEnabled: false,
+      }
+    }),
+  ],
+  footer: Component.Footer({
+    links: {
+      License: "/license",
+    },
+  }),
+}
+
+// components for pages that display a single page (e.g. a single note)
+export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.TagList(),
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [
+    Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
+  ],
+}
+
+// components for pages that display lists of pages  (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [],
+}
